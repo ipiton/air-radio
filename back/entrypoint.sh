6b17c4c38bf344308baab6ab549664f200f397c2
vit <vitaliysemenov@hotmail.com>
2020-08-07 16:10:46 +0400

    Edit entry script for edit env.

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
+set_val() {
+    if [ -n "$2" ]; then
+        echo "set '$2' to '$1'"
+        sed -i "s#<$2>[^<]*<\/$2>#<$2>$1<\/$2>#g" /ezstream/ezstream.xml
+    else
+        echo "Setting for '$1' is missing, skipping." >&2
+    fi
+}
 
-if [ -n "$PLAYLIST" ]; then
-    sed -i "s#<filename>[^<]*<\/filename>#<filename>$PLAYLIST<\/filename>#g" /ezstream/ezstream.xml
-fi
-if [ -n "$MOUNTPOINT" ]; then
-    sed -i "s#<mountpoint>[^<]*<\/mountpoint>#<mountpoint>$MOUNTPOINT<\/mountpoint>#g" /ezstream/ezstream.xml
-fi
+set_val $PLAYLIST filename
+set_val $MOUNTPOINT mountpoint
+set_val "$STREAM_NAME" stream_name
+set_val $STREAM_URL stream_url
+set_val "$STREAM_GENRE" stream_genre
+set_val "$STREAM_DESC" stream_description
 exec "$@"
-
-
-if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
-    sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast.xml
-fi
