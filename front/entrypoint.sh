ba05a276585c538a85b3bf3525397bb8099f4ab7
Vit <i_piton@mail.ru>
2020-08-28 18:33:25 +0400

    Добавил переменную хоста, для отдачи файлов плейлиста

---
+++ front/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set_val() {
+    if [ -n "$2" ]; then
+        echo "set '$2' to '$1'"
+        sed -i "s#<$2>[^<]*<\/$2>#<$2>$1<\/$2>#g" /ezstream/ezstream.xml
+    else
+        echo "Setting for '$1' is missing, skipping." >&2
+    fi
+}
+
+set_val $HOSTNAME hostname
+
+exec "$@"
