48779ba82d06fe1b3a7757ccf0637bc643d847b8
vit <vitaliysemenov@hotmail.com>
2020-09-02 09:53:10 +0400

    Fix entrypoint frontend(icecast)

--- front/entrypoint.sh
+++ front/entrypoint.sh
@@ -2,7 +2,7 @@
 set_val() {
     if [ -n "$2" ]; then
         echo "set '$2' to '$1'"
-        sed -i "s#<$2>[^<]*<\/$2>#<$2>$1<\/$2>#g" /ezstream/ezstream.xml
+        sed -i "s#<$2>[^<]*<\/$2>#<$2>$1<\/$2>#g" /etc/icecast.xml
     else
         echo "Setting for '$1' is missing, skipping." >&2
     fi
